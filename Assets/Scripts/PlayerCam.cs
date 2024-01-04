5a9232dff74848d4016d47ab4013e05ae5d8d191
AamirC22 <91503434+AamirC22@users.noreply.github.com>
2024-01-04 14:46:33 +0000

    ADded sensitivity X and Y sliders to settings

--- Assets/Scripts/PlayerCam.cs
+++ Assets/Scripts/PlayerCam.cs
@@ -1,22 +1,23 @@
-    using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
-using System.Security.Cryptography;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerCam : MonoBehaviour
 {
-
-    public float sensX;
-    public float sensY;
-
+    public float sensX = 100f;
+    public float sensY = 100f;
+    public Slider XSlider;
+    public Slider YSlider;
     public Transform orientation;
-
     float xRotation;
     float yRotation;
 
     // Start is called before the first frame update
     private void Start()
     {
+        XSlider.value = PlayerPrefs.GetFloat("XSens", sensX);
+        YSlider.value = PlayerPrefs.GetFloat("YSens", sensY);
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
@@ -35,5 +36,17 @@ public class PlayerCam : MonoBehaviour
         // rotate cam and orientation
         transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
         orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+    }
+
+    public void OnXChanged()
+    {
+        sensX = XSlider.value;
+        PlayerPrefs.SetFloat("XSens", sensX);
+    }
+
+    public void OnYChanged()
+    {
+        sensY = YSlider.value;
+        PlayerPrefs.SetFloat("YSens", sensY);
     }
 }
